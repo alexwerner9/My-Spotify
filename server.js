ac7aec1f111467ac3fb96640d6698a023b708a17
Alex <alexwerner9@gmail.com>
2021-07-27 19:13:13 -0700

    Fixes

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 var http = require('http');
 var nodefetch = require('node-fetch');
 var express = require('express');
+var path = require('path');
 
 express()
-    .get('/', (req, res) => res.render(body.html))
+    .use(express.static(__dirname))
+    .set('views', __dirname)
+    .set('view engine', 'ejs')
+    .get('/', (req, res) => res.render('body'))
     .listen(process.env.PORT || 80, () => console.log('Listening'));
