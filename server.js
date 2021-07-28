baa6cf953426292a8b4c6ba57320f02a2bf4f30c
Alex <alexwerner9@gmail.com>
2021-07-27 21:55:24 -0700

    C

--- server.js
+++ server.js
@@ -29,5 +29,22 @@ express()
 function parseSpotifyResponse(req, s) {
     var url = new URL(req.protocol + '://' + req.get('host') + req.originalUrl);
     code = url.searchParams.get("code");
+    console.log('Code retrieved');
+    requestAccessToken();
     console.log(code);
+}
+
+function requestAccessToken() {
+    fetch('https://accounts.spotify.com/api/token', {
+    method: 'POST',
+    body: new URLSearchParams({
+        'grant_type': 'authorization_code',
+        'code': code,
+        'redirect_uri': 'https%3A%2F%2Fthawing-island-42941.herokuapp.com%2Fspotifyauth%2F',
+        'client_id': 'd8f5f88f01a644ee803480f73bda4708',
+        'client_secret': 'ccae11a4e8004f569057ac21549afdbe'
+        })
+    })
+    .then(response => console.log(response.json()));
+
 }
