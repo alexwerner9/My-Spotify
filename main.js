f612beac3277060d26db70d02dde0fd438f5f3a2
Alex <alexwerner9@gmail.com>
2021-07-28 20:04:25 -0700

    C

--- main.js
+++ main.js
@@ -1,11 +1,3 @@
-console.log("beginning");
+var fetch = require('fetch');
 
-console.log(encodeURIComponent('https://www.google.com/'));
-
-let url = 'https://accounts.spotify.com/authorize?';
-let params = new URLSearchParams({client_id:'d8f5f88f01a644ee803480f73bda4708',
-                                  response_type:'code',
-                                  redirect_uri:encodeURIComponent('http://localhost:8080/')});
-
-console.log(url+params);
-console.log(encodeURIComponent('http://localhost:8080/'));
+console.log('runing js');
