7a56c1d4abb32c93c4267aec2e3cbba46cb58d63
Alex <alexwerner9@gmail.com>
2021-07-29 17:53:51 -0700

    C

--- client.js
+++ client.js
@@ -4,9 +4,9 @@ var intervalId = window.setInterval(function(){
         url:'https://thawing-island-42941.herokuapp.com/updatesong',
         success:function(response) {
             $('#song').html(response.song);
+            $('#song').attr('href', response.songURL);
             $('#artist').html(response.artist);
             $('#albumIMG').attr('src', response.albumIMG);
-            $('#song').attr('href', response.songURL);
             console.log(response.songURL);
         },
         error:function(response) {
