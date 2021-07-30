91e5d58a90f5e9996e4c477fbe27d315d606cda6
Alex <alexwerner9@gmail.com>
2021-07-29 17:06:27 -0700

    C

---
+++ client.js
@@ -0,0 +1,15 @@
+var intervalId = window.setInterval(function(){
+    console.log("inside AJAX");
+    $.ajax({
+        type:'GET',
+        url:'https://thawing-island-42941.herokuapp.com/updatesong',
+        success:function(response) {
+            $('#song').html(response.song);
+            $('#artist').html(response.artist);
+            $('#albumIMG').attr('src', response.albumIMG);
+        },
+        error:function(response) {
+            console.log("error in AJAX");
+        }
+    });
+}, 1000);
