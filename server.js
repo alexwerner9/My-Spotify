5768cab6086c36dcf31d20095b60de50931c8a70
Alex <alexwerner9@gmail.com>
2021-08-05 11:20:14 -0700

    Rollback

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
+var http = require('http');
 var fetch = require('node-fetch');
 var express = require('express');
-var app = express();
 var cors = require('cors');
+var path = require('path');
 const { renameSync } = require('fs');
 const { response } = require('express');
 
@@ -14,44 +15,38 @@ var access_token = '';
 var refresh_token = '';
 var songData;
 
-app.use(express.static(__dirname + '/front_end'));
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({extended : true}));
-app.set('views', __dirname);
-app.set('view engine', 'ejs');
-
-app.get('/spotifyauth/' + id, function(req, res) {
+express()
+    .use(express.static(__dirname + '/front_end'))
+    .use(cors())
+    .use(express.json())
+    .set('views', __dirname)
+    .set('view engine', 'ejs')
+    .get('/spotifyauth/' + id, function(req, res) {
         setInterval(showCurrentSong, 1000);
         res.render('front_end/requestauth');
-    });
-app.get('/updatesong', function(req,res) {
+    })
+    .get('/updatesong', function(req,res) {
         res.send({
             'song':songData.item.name,
             'artist':songData.item.album.artists[0].name,
             'albumIMG':songData.item.album.images[1].url,
             'songURL':songData.item.album.external_urls.spotify
         });
-    });
-
-app.get('/getaccesstoken', function(req,res) {
-        res.send(access_token);
-    });
-
-app.get('/', function(req,res) {
+    })
+    .get('/getaccesstoken', function(req,res) {
+        res.send(access_token)
+    })
+    .get('/', function(req,res) {
         console.log("New visitor / refresh");
         if(!access_token) {
             parseSpotifyResponse(req);
         }
         res.render('front_end/body');
         res.end();
-    });
-
-app.post('/search', function(req,res) {
-    
-});
-
-app.listen(process.env.PORT || 80, () => console.log('Listening'));
+    })
+    .post('/search', function(req,res) {
+    })
+    .listen(process.env.PORT || 80, () => console.log('Listening'));
 
 function parseSpotifyResponse(req) {
     var url = new URL(req.protocol + '://' + req.get('host') + req.originalUrl);
@@ -105,7 +100,6 @@ function requestRefreshToken() {
 }
 
 function showCurrentSong() {
-    console.log("fetching current song");
     fetch('https://api.spotify.com/v1/me/player', {
         headers: {
             'Authorization':'Bearer ' + access_token
@@ -114,7 +108,6 @@ function showCurrentSong() {
     })
     .then(response => response.json())
     .then(function(data) {
-        console.log(data);
         songData = data;
     });
 }
